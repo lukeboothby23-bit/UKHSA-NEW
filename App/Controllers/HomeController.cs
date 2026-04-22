4f2396e604bb567d22695d3e3aceb706bdba983d
Luke Boothby <lukeboothby23@gmail.com>
2026-04-22 12:00:48 +0100

    Home?

--- App/Controllers/HomeController.cs
+++ App/Controllers/HomeController.cs
@@ -11,6 +11,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Home()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
