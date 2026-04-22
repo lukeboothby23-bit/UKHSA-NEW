b1c5d10c6f7322c06b2fed76e3e6e22233a28fbe
Luke Boothby <lukeboothby23@gmail.com>
2026-04-22 15:18:13 +0100

    added layout from old project

--- App/obj/Debug/net10.0/App.AssemblyInfo.cs
+++ App/obj/Debug/net10.0/App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bb724f9a753d2e33785d88d3d96066783ae9340")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f7a2ced5b7e9764ee483f75d26c73a5b06a2ea5")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
