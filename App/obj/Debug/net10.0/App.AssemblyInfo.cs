42e01573d89b2ca3a48a261c0018889150c118fa
Luke Boothby <lukeboothby23@gmail.com>
2026-04-22 14:39:08 +0100

    imported views may need soime fixing

--- App/obj/Debug/net10.0/App.AssemblyInfo.cs
+++ App/obj/Debug/net10.0/App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17e64390706002d5213e0face2e05ca2ff35536c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aba796089e9d7aff8112c6e06fb37b222448123e")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
